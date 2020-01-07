f0e67098635c73c85253ca63eef2cc02d64f8256
Erik-joh <erik.johannesson0@gmail.com>
2020-01-07 15:12:59 +0100

    added path to avatar in user

--- app/users/uploadAvatar.php
+++ app/users/uploadAvatar.php
@@ -6,7 +6,9 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_FILES['avatar'])) {
     $avatar = $_FILES['avatar'];
-    $destination = __DIR__ . '/../../assets/images/avatars/' . date('ymd') . '-' . $avatar['name'];
-
+    $id = $_SESSION['user']['id'];
+    $destination = filter_var(__DIR__ . '/../../assets/images/avatars/' . date('ymd') . '-' . $avatar['name'], FILTER_SANITIZE_STRING);
+    $statement = $pdo->prepare('UPDATE users SET avatar=:avatar WHERE id =:id');
+    $statement->execute([':avatar' => $destination, ':id' => $id]);
     move_uploaded_file($avatar['tmp_name'], $destination);
 }
