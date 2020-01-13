3c5d7781b23fb5de4cdc9ec3ef7be76b8ddcc943
Erik-joh <erik.johannesson0@gmail.com>
2020-01-13 09:32:38 +0100

    ableto store posts in database

--- app/posts/store.php
+++ app/posts/store.php
@@ -9,7 +9,7 @@ if (isset($_POST['title'], $_FILES['image'], $_POST['description'])) {
     $image = $_FILES['image'];
     $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
     $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
-    $id = $_SESSION['user']['id'];
+    $id = (int) $_SESSION['user']['id'];
 
     if ($image['type'] !== 'image/png') {
         die(var_dump('error type'));
@@ -18,10 +18,14 @@ if (isset($_POST['title'], $_FILES['image'], $_POST['description'])) {
     }
     $destination = filter_var('/assets/images/postsImages/' . date('ymd') . '-' . $image['name'], FILTER_SANITIZE_STRING);
 
-    $statement = $pdo->prepare('INSERT INTO posts (title,image,content,users_id) VALUES (:title,:image,:content,:id)');
+    // $statement = $pdo->prepare('INSERT INTO posts (title,image,content,users_id) VALUES ("test","image","content",2)');
+    // $statement->execute();
+    $statement = $pdo->prepare('INSERT INTO posts (title,content,users_id,image) VALUES (:title,:content,:id,:image)');
 
-    $statement->execute([':title' => $title, ':image' => $destination, ':description' => $description, ':id' => $id]);
+    $statement->execute([':title' => $title, ':image' => $destination, ':content' => $description, ':id' => $id]);
+
     move_uploaded_file($image['tmp_name'], __DIR__ . '/../../' . $destination);
+    errorMessage('Posted');
     redirect('/post.php');
 }
 errorMessage('Atleast one field missing');
