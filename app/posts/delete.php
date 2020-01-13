80df17f8e0072c8ec4ff7843e21bdfa89a6ab8ab
Erik-joh <erik.johannesson0@gmail.com>
2020-01-13 12:55:53 +0100

    added ability to delete a post

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -2,8 +2,16 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/../autoload.php';
+require __DIR__ . '/../autoload.php';
 
-// In this file we delete new posts in the database.
+$postId = $_GET['id'];
 
-redirect('/');
+$statement = $pdo->prepare('SELECT image FROM posts WHERE id=:postId AND users_id=:userId');
+$statement->execute([':postId' => $postId, ':userId' => $id]);
+$oldDestination = $statement->fetch(PDO::FETCH_ASSOC);
+unlink(__DIR__ . '/../../' . $oldDestination['image']);
+
+$statement = $pdo->prepare('DELETE FROM posts WHERE id=:postId AND users_id=:userId');
+$statement->execute([':postId' => $postId, ':userId' => $id]);
+
+redirect('/myPosts.php');
